ee7f2115b7da050bf8503d94734ddef93bfe9263
bishnudev35 <bishnudevsardar35@gmail.com>
2024-04-05 19:38:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Certificate from './Certificate'
+
+
+function App() {
+ 
+
+  return (
+  <div>
+    <h1 className='text-2xl'>welcome to class connect</h1>
+    <Certificate/>
+  </div>
+  )
+}
+
+export default App
