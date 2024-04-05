bc0f09c1f2d6080f6e7f6f25f97e8fd304d86a43
bishnudev35 <bishnudevsardar35@gmail.com>
2024-04-06 02:44:28 +0530

    "ok"

--- src/Certificate.jsx
+++ src/Certificate.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useCallback, useRef } from "react";
 import { toPng } from 'html-to-image';
-import './certificate.css'
-import certificateTemplate from './certificate.png'
+import './certificate.css';
+import certificateTemplate from './certificate.png';
 
 const Certificate = () => {
     const ref = useRef(null);
@@ -23,22 +23,16 @@ const Certificate = () => {
                 console.log(err);
             });
     }, [ref]);
-
     return (
-        <div className="text-black absolute" style={{margin:"0px",padding:"0px"}}>
-            <input className="m-9 absolute rounded-lg" style={{border:"2px solid black"}} type='text' placeholder='enter name' value={name} onChange={(e) => setName(e.target.value)} />
-            <div className="size-1/2 m-64 mt-16 " ref={ref} style={{position:"absolute",  height:"600px", width:"600px", top:"50px" }}>
-
-                <div style={{ textAlign: "center",  }}>
-                    <img src={certificateTemplate} height={400} style={{ display: "inline-block" }} />
-                </div>
-                
-                <div className="conti text-xl font-medium" style={{ position:"relative", top:"-239px", bottom:"51px", left:"273px" }}>
-                   
-                    <h1 className="mb-10">{name}</h1>
-                </div>
+        <div className="container">
+            <input className="name-input" type='text' placeholder='Enter Name' value={name} onChange={(e) => setName(e.target.value)} />
+           
+            <div className="certificate-container" ref={ref}>
+                <img src={certificateTemplate} alt="Certificate Template" className="certificate-image" />
+                <div className="name-container text-">{name}</div>
             </div>
-            <button  style={{ position:"relative", bottom:"-600px",left:"573px"}} className="bg-gray-700 rounded-xl p-3 flex justify-center" onClick={onButtonClick}>Click me</button>
+            
+            <button className="download-button" onClick={onButtonClick}>Download Certificate</button>
         </div>
     );
 }
